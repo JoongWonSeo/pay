f8f27586c8de2a14befa849a8aa5938014bd98c0
Viktor Shen <viktorshenofficial@gmail.com>
2025-11-15 15:26:37 -0800

    new evaluation

--- backend/pay/synced.py
+++ backend/pay/synced.py
@@ -1,7 +1,8 @@
 # synced object
 import asyncio
 import logging
-from typing import override
+from datetime import datetime
+from typing import Literal, override
 
 from ws_sync import SessionState, SyncedAsCamelCase, remote_action, sync_all
 
@@ -11,9 +12,47 @@ from pay.tiktok import TiktokChannel, TiktokPost, TiktokService
 logger = logging.getLogger(__name__)
 
 
+class TiktokPostEvaluation(Model):
+    id: str | None = None
+    """id as a internal integer"""
+    product_mentioned: bool | None = None
+    """Whether the product is mentioned in the post"""
+    prominence_of_product: Literal["high", "medium", "low"] | None = None
+    """How prominent the product is in the post"""
+    target_group_fit: Literal["high", "medium", "low"] | None = None
+    """How well the post fits the target group"""
+    post_type: (
+        Literal["demo", "review", "product recommendation", "trend", "other"] | None
+    ) = None
+    """Type of post"""
+    estimated_ctr: float | None = None
+    """Estimated CTR (typically around 0.2% to 5%)"""
+    determined_price_per_1k: float | None = None
+    """Determined price per 1K views"""
+    determined_payout: float | None = None
+    """Determined payout based on all factors"""
+    date_evaluated: datetime | None = None
+    """Date evaluated"""
+    evaluation_text: str | None = None
+    """Evaluation"""
+
+
 class BackendState(SessionState, SyncedAsCamelCase, Model):
     channels: list[TiktokChannel] = []
     posts_by_channel_id: dict[str, list[TiktokPost]] = {}
+    post_evaluations: dict[str, TiktokPostEvaluation] = {
+        # mocked data
+        "1": TiktokPostEvaluation(
+            id="1",
+            product_mentioned=True,
+            prominence_of_product="high",
+            target_group_fit="high",
+            post_type="demo",
+            estimated_ctr=0.2,
+        ),
+    }
+    """Post evaluations by post id"""
+    post_payouts: dict[str, float] = {}
 
     @sync_all()
     def model_post_init(self, _):
